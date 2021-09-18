b1ad51da248673ba0259320be31fe5b36d0c5757
Saadat Sayem <asm.sayem@brightbrains.ae>
2021-09-19 02:25:10 +0600

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMM_App"
+include(":KMM_Android_App")
+include(":shared")
